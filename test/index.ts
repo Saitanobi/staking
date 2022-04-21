de8b088759d82fbf1ac47833be12542fd75388b8
dperk6 <daniel@perklin.ca>
2022-04-21 16:46:22 -0400

    test additional tokens

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,13 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { approve, getBalance, makeSwap } from "../scripts/utils";
+import { approve, getBalance, makeSwap, transfer } from "../scripts/utils";
 
 const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 const SAITANOBI = "0x5e9f35e8163c44cd7e606bdd716abed32ad2f1c6";
 const SAITAMA = "0x8b3192f5eebd8579568a2ed41e6feb402f93f73f";
 const SHIBNOBI = "0xab167E816E4d76089119900e941BEfdfA37d6b32";
+const SHIB = "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE";
 
 describe("MultiRewardsStake", function () {
   it("Should allow add tokens, remove tokens, stake, and withdraw", async function () {
@@ -24,6 +25,7 @@ describe("MultiRewardsStake", function () {
 
     await makeSwap(accounts[0], [WETH, SAITAMA], '4.0');
     await makeSwap(accounts[0], [WETH, SHIBNOBI], '4.0');
+    await makeSwap(accounts[0], [WETH, SHIB], '4.0');
     await makeSwap(accounts[0], [WETH, SAITANOBI], '1.0');
     await makeSwap(accounts[1], [WETH, SAITANOBI], '1.0');
 
@@ -52,6 +54,7 @@ describe("MultiRewardsStake", function () {
 
     saitamaBalance = await getBalance(SAITAMA, accounts[0].address);
     shibnobiBalance = await getBalance(SHIBNOBI, accounts[0].address);
+    const shibBalance = await getBalance(SHIB, accounts[0].address);
 
     await stake.depositRewardTokens([saitamaBalance, shibnobiBalance]);
 
@@ -77,18 +80,33 @@ describe("MultiRewardsStake", function () {
     await ethers.provider.send('evm_mine', []);
     await ethers.provider.send('evm_mine', []);
     await ethers.provider.send('evm_mine', []);
-    
-    await stake.connect(accounts[0]).exit();
-    await stake.connect(accounts[1]).exit();
 
-    const leftover = await stake.totalSupply();
+    console.log(await stake.rewardPerToken());
 
-    expect(Number(leftover)).to.equal(0);
+    await transfer(SHIB, accounts[0], stake.address, shibBalance);
 
-    await stake.connect(accounts[0]).transferOwnership(accounts[1].address);
+    const contractShibBalance = await getBalance(SHIB, stake.address);
 
+    // await stake.addRewardToken(SHIB);
+
+    // await ethers.provider.send('evm_increaseTime', [2628000]);
     await ethers.provider.send('evm_mine', []);
+    
+    // await stake.connect(accounts[0]).exit();
+    // await stake.connect(accounts[1]).exit();
 
-    expect(await stake.owner()).to.equal(accounts[1].address);
+    // console.log(await stake.getRewardTokens());
+    console.log(await stake.rewardPerToken());
+    console.log(await getBalance(SHIB, accounts[1].address));
+
+    // const leftover = await stake.totalSupply();
+
+    // expect(Number(leftover)).to.equal(0);
+
+    // await stake.connect(accounts[0]).transferOwnership(accounts[1].address);
+
+    // await ethers.provider.send('evm_mine', []);
+
+    // expect(await stake.owner()).to.equal(accounts[1].address);
   });
 });
